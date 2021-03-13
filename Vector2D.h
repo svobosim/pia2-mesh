a2d3418b593e29a69e13d48ad9dd19578387ad8f
kreuzter <61558471+kreuzter@users.noreply.github.com>
2021-03-13 17:21:50 +0100

    Update Vector2D.h

--- Vector2D.h
+++ Vector2D.h
@@ -7,12 +7,12 @@ class Vector2D {
 public:
     Vector2D(double _x, double _y) : x(_x), y(_y) {};
     Vector2D(Point const& u, Point const& v) : x(v.x - u.x), y(v.y - u.y) {};
-    //friend Vector2D operator+(Vector2D const& v, Vector2D const& u);
-    //friend Vector2D operator-(Vector2D const& v, Vector2D const& u);
+    friend Vector2D operator+(Vector2D const& v, Vector2D const& u);
+    friend Vector2D operator-(Vector2D const& v, Vector2D const& u);
     //friend Vector2D operator*(double a, Vector2D const& u);
     //friend Vector2D operator*(Vector2D const& u, double a);
     //friend Vector2D operator/(Vector2D const& u, double a);
-    //friend double dot(Vector2D const& v, Vector2D const& u);
+    friend double dot(Vector2D const& v, Vector2D const& u);
     double x, y;
 };
 
